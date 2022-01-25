4abb63c81231282f0d8ec920d9892549729355ba
Lan <h.baolan20020@gmail.com>
2022-01-26 04:06:52 +0700

    Connect MySql and use some query function

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path')
 const express = require('express');
 const bodyParser = require('body-parser');
 const errorControllers = require('./controllers/error');
+const db = require('./util/database');
+
 
 const app = express();
 
@@ -12,6 +14,7 @@ app.set('views', 'views')
 
 const adminRoutes = require('./routes/admin')
 const shopRoutes = require('./routes/shop')
+
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public'))); // It serves static file
 
